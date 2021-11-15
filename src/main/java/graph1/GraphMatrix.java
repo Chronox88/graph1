5ad57efb6744fa7d4ec5b5ba1c1bd8146a298612
Kevin Setyadi <kevinstydi@gmail.com>
2021-11-15 08:14:13 +0700

    initial commit

--- src/main/java/graph1/GraphMatrix.java
+++ src/main/java/graph1/GraphMatrix.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package graph1;
-
-import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  *
- * @author djeq
+ * @author Kevin Setyadi
  */
 public class GraphMatrix {
 
